b8cd1dd63ace8a6f192a670941f2f2466df15040
Zhang <Xiekang.Zhang@tedi.com>
2025-04-17 17:05:41 +0200

    other way

--- terraform/learn-terraform-gcp/main.tf
+++ terraform/learn-terraform-gcp/main.tf
@@ -17,25 +17,73 @@ provider "google" {
   zone    = var.zone
 }
 
-// vm with public access works!
-resource "google_compute_instance" "vm_instance" {
+// vm without public access
+resource "google_storage_bucket_object" "startup_script_object" {
+  bucket = "script/my-script.sh"
+  name   = "script"
+  source = "./my_script.sh"
+}
+# 1. create a VPC network
+resource "google_compute_network" "vpc_network" {
+  name                    = "internal-network"
+  auto_create_subnetworks = false
+}
+
+# 2. create a subnetwork
+resource "google_compute_subnetwork" "subnet" {
+  name          = "internal-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  network       = google_compute_network.vpc_network.id
+  region        = var.region
+}
+
+# 3. firewall rule to allow IAP access
+resource "google_compute_firewall" "allow_iap" {
+  name    = "allow-iap-ssh"
+  network = google_compute_network.vpc_network.id
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  source_ranges = ["35.235.240.0/20", "35.235.252.0/22", "35.235.1.0/24", "35.235.128.0/20"]
+  target_tags = ["iap-allowed"]
+}
+
+/**
+# 4. reserve an internal IP address --> Optional
+resource "google_compute_address" "internal" {
+  name       = "internal-ip"
+  subnetwork = google_compute_subnetwork.subnet.id
+  region     = var.region
+}**/
+
+# 5. create the VM with only an internal IP and startup script
+resource "google_compute_instance" "vm_internal_ip" {
   machine_type = "e2-medium"
-  name         = "terraform-instance-public-access"
-  tags = ["test", "scheduled"]
+  name         = "terraform-instance-internal-access"
+  zone         = var.zone
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
       size  = 10
     }
   }
+
   metadata = {
-    ssh-keys = "${var.ssh_username}:${file("./.ssh/gcp-key.pub")}"
+    ssh-keys       = "${var.ssh_username}:${file("./.ssh/gcp-key.pub")}"
+    startup-script = <<-EOF
+      #!/bin/bash
+      BUCKET_NAME
+      chmod +x /home/xiekang.zhang/my_script.sh
+      (crontab -l 2>/dev/null; echo \"0,30 * * * * /home/xiekang.zhang/my_script.sh\") | crontab -
+    EOF
   }
 
   network_interface {
-    network = "default"
-    access_config {}
+    subnetwork = google_compute_subnetwork.subnet.id
   }
+
   service_account {
     email = "130065393661-compute@developer.gserviceaccount.com"
     scopes = [
@@ -43,37 +91,5 @@ resource "google_compute_instance" "vm_instance" {
       "https://www.googleapis.com/auth/compute"
     ]
   }
-  allow_stopping_for_update = true
+  tags = ["iap-allowed"]
 }
-
-resource "null_resource" "upload_script" {
-  depends_on = [google_compute_instance.vm_instance]
-
-  provisioner "file" {
-    source      = "my_script.sh"
-    destination = "/home/${var.ssh_username}/my_script.sh"
-    connection {
-      type    = "ssh"
-      user    = var.ssh_username
-      private_key = file("./.ssh/gcp-key")
-      host    = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
-      timeout = "1m"
-    }
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /home/${var.ssh_username}/my_script.sh",
-      "/home/${var.ssh_username}/my_script.sh",
-      "(crontab -l 2>/dev/null; echo \"0,30 * * * * /home/${var.ssh_username}/my_script.sh\") | crontab -"
-    ]
-    connection {
-      type    = "ssh"
-      user    = var.ssh_username
-      private_key = file("./.ssh/gcp-key")
-      host    = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
-      timeout = "1m"
-    }
-  }
-}
-
-// vm without public access
